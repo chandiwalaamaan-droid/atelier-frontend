683ce690cc69666e8bd7013e5d29ed819cecd6bc
chandiwalaamaan-droid <chandiwalaamaan@gmail.com>
2026-08-02 18:40:22 +0530

    Delete prisma directory

--- prisma/migrations/20260725130000_add_billing_fk/migration.sql
+++
@@ -1,7 +0,0 @@
--- Split out from 20260725130000_add_billing: PaymentOrder needs to already
--- be unlocked, which only takes effect once that migration's transaction
--- has fully committed.
-
-ALTER TABLE "PaymentOrder"
-    ADD CONSTRAINT "PaymentOrder_userId_fkey"
-    FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
