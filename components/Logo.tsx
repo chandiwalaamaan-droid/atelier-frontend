bd83571b50d7f987a91dba22978d6c1171e16eb6
chandiwalaamaan-droid <chandiwalaamaan@gmail.com>
2026-08-09 18:14:42 +0530

    Add files via upload

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,15 +1,17 @@
-import Image from "next/image";
-
 export default function Logo({ className = "", size = 32 }: { className?: string; size?: number }) {
   return (
-    <Image
+    // Plain <img>, not next/image: a fixed-size logo doesn't need responsive
+    // optimization, and routing it through Next's image pipeline on Netlify
+    // (which needs their image-optimization function working correctly)
+    // was showing up as a broken image icon.
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
       src="/logo.png"
       alt="Rolichat"
       width={size}
       height={size}
       className={className}
       style={{ width: size, height: size, objectFit: "contain" }}
-      priority
     />
   );
 }
